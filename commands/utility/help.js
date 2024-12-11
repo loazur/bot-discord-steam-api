e972a563dc9e3285794fbd37f16088a44d7c0749
loazur <clement.bounaix@gmail.com>
2024-12-11 20:13:25 +0100

    Embed Message dans /help

--- commands/utility/help.js
+++ commands/utility/help.js
@@ -1,4 +1,4 @@
-const { SlashCommandBuilder, MessageFlags, PermissionFlagsBits } = require("discord.js");
+const { SlashCommandBuilder, MessageFlags, PermissionFlagsBits, EmbedBuilder } = require("discord.js");
 
 module.exports = {
     category: "utility",
@@ -9,14 +9,24 @@ module.exports = {
     async execute(interaction) {
         const commandsList = interaction.client.commands;
 		
-        // ! utiliser un embed
-        let textHelp = "***__Help :__*** \n";
+        const helpEmbed = new EmbedBuilder()
+            .setColor("White")
+            .setTitle('__Help:__')
+            .setDescription(" ")
+            .addFields({ name: '\u200b', value: '*D\'autres commandes seront ajoutées prochainement.*' })
+            .setThumbnail(`${interaction.client.user.avatarURL()}`)
+            .setTimestamp()
+            .setFooter({ text: `${interaction.client.user.username}`, iconURL: `${interaction.client.user.avatarURL()}` });
+
+        let textHelp = "";
 
         commandsList.forEach(command => {
             textHelp += `- **${command.data.name}** (${command.category}) | ${command.data.description} \n`;
         });
         
-        await interaction.reply(textHelp);
+        helpEmbed.setDescription(textHelp);
+
+        await interaction.reply({embeds: [helpEmbed]});
 
     }
 
